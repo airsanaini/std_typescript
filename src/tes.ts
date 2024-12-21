f5eb4579a0d1dbfd1fa510766ec8ba783941334e
airsanaini <airsanaini@gmail.com>
2024-12-21 17:29:46 +0700

    1

--- src/tes.ts
+++ src/tes.ts
@@ -1,3 +1,9 @@
-// Ini adalah contoh typescript
+let nomor: number = 123_456_789;
+let nama: string = "Arkha";
+let apakah_botak: boolean = true;
+let obj: { nama: string, usia: number } = { nama: "John", usia: 25 };
 
-console.log("Hello Omi dan Arkha, Obi dirumah menunggu");
+console.log(nomor);
+console.log(nama);
+console.log(apakah_botak);
+console.log(obj);
