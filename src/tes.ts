d11fcecd3362d369d143e228ccfb56074151c117
airsanaini <airsanaini@gmail.com>
2024-12-21 19:25:32 +0700

    1

--- src/tes.ts
+++
@@ -1,9 +0,0 @@
-let nomor: number = 123_456_789;
-let nama: string = "Arkha";
-let apakah_botak: boolean = true;
-let obj: { nama: string, usia: number } = { nama: "John", usia: 25 };
-
-console.log(nomor);
-console.log(nama);
-console.log(apakah_botak);
-console.log(obj);
